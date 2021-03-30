7b2981e934f06c03dd92408db08508906d7d8aa7
Angelhdz <angel@oshuntrading.com.mx>
2021-03-30 16:40:22 -0600

    modelos de tablas de datos

---
+++ app/Models/Empresas_colaboradore.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Colaboradores_telefono as CTel;
+
+class Empresas_colaboradore extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+      'id',
+      'id_empresa',
+      'nombre',
+      'apellido_paterno',
+      'apellido_materno',
+      'area_empresarial',
+      'puesto'
+    ];
+
+    public function telefonos_CTel()
+    {
+        return $this->hasMany(CTel::class, 'id_colaborador', 'id');
+    }
+}
