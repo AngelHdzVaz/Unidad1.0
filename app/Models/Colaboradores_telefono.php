9a77dc43a4c802f8a38e34da7577c8027b3e59d8
Angelhdz <angel@oshuntrading.com.mx>
2021-03-30 16:39:26 -0600

    modelos de tablas de datos

---
+++ app/Models/Colaboradores_telefono.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Colaboradores_telefono extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+      'id',
+      'id_colaborador',
+      'telefono'
+    ];
+}
