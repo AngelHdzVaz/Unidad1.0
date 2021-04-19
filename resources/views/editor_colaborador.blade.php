6c490e4a0003b5758cc24a2875ef82cfbe93f30e
Angelhdz <angel@oshuntrading.com.mx>
2021-04-19 16:56:48 -0500

    editor datos colaborador

--- resources/views/editor_colaborador.blade.php
+++ resources/views/editor_colaborador.blade.php
@@ -23,7 +23,7 @@
 
                 </div>
                 <div class="card-body  ">
-                    <form method="POST" action="{{ route('RegistrarColaborador') }}">
+                    <form method="POST" action="{{ route('ActualizarColaborador',['correo'=>$correo]) }}">
                         @csrf
 
                         <div class="form-group row ">
@@ -90,9 +90,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-success align-center">
-                                    {{ __('Guardar') }}
-                                </button>
+                                <button class="btn" type="submit" >Guardar </button>
 
                             </div>
                         </div>
